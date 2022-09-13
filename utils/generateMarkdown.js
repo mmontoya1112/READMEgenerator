aa50cfce86238d0706952602f7dd3727b34a463b
michael montoya <michaelmontoya@michaels-MacBook-Pro.local>
2022-09-13 15:29:35 -0700

    added function to pull the answers from inquirer into index html

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -34,6 +34,11 @@ inquirer
     },
     {
       type: 'input',
+      name: 'description',
+      message: 'describe your project',
+    },
+    {
+      type: 'input',
       name: 'instructions',
       message: 'what are the instructions for your project',
     },
@@ -63,23 +68,30 @@ inquirer
       message: 'questions about future develpment?',
     },
   ])
+  .then((answers) => {
+    const READMEhtml = createReadme(answers);
+
+    fs.writeFile('index.html', READMEhtml, (err) =>
+      err ? console.log(err) : console.log('created professional README')
+    );
+  });
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+// function generateMarkdown(data) {
+//   return `# ${data.title}
 
-`;
-}
+// `;
+// }
 
-module.exports = generateMarkdown;
+// module.exports = generateMarkdown;
