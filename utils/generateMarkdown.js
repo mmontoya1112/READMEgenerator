dbc04b1dc6793d217b1fe918b53552b0934db1c7
michael montoya <michaelmontoya@michaels-MacBook-Pro.local>
2022-09-13 14:12:46 -0700

    required node modules

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,6 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
