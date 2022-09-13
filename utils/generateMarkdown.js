60917511064a5df9ef27f977f93f395393e1191c
michael montoya <michaelmontoya@michaels-MacBook-Pro.local>
2022-09-13 15:07:46 -0700

    added a generate html const and put in the index html into it

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,30 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+const createHTML = ({project, description, instructions, usage, contents, install, contributors, questions}) =>
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>README</title>
+</head>
+<body>
+        <div class="container">
+          <h1 class="display-4">the project is called ${project}</h1>
+          <h2 class="lead" >the project description is ${description}</h2>
+          <h3>follow these instructions: ${instructions}</h3>
+          <ul class="list-group">
+            <li class="list-group-item">When to use this application: ${usage}</li>
+            <li class="list-group-item">table of contents: ${contents}</li>
+            <li class="list-group-item">installation process: ${install}</li>
+            <li class="list-group-item">contributors:  ${contributors} </li>
+            <li class="list-group-item">questions: ${questions}</li>
+          </ul>
+        </div>
+</body>
+</html>`;
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
