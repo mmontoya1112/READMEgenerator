6e60692ebd8f049b6096f5600f419220dc2bc1d9
michael montoya <michaelmontoya@michaels-MacBook-Pro.local>
2022-09-13 16:03:10 -0700

    commented out some js to and created new const to write a file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,30 +1,33 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const util = require('util');
+const writeFile = util.promisify(fs.writeREADME)
 
-const createReadme = ({project, description, instructions, usage, contents, install, contributors, questions}) =>
-`<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>README</title>
-</head>
-<body>
-        <div class="container">
-          <h1 class="display-4">the project is called ${project}</h1>
-          <h2 class="lead" >the project description is ${description}</h2>
-          <h3>follow these instructions: ${instructions}</h3>
-          <ul class="list-group">
-            <li class="list-group-item">When to use this application: ${usage}</li>
-            <li class="list-group-item">table of contents: ${contents}</li>
-            <li class="list-group-item">installation process: ${install}</li>
-            <li class="list-group-item">contributors:  ${contributors} </li>
-            <li class="list-group-item">questions: ${questions}</li>
-          </ul>
-        </div>
-</body>
-</html>`;
+// const createReadme = ({project, description, instructions, usage, contents, install, contributors, questions}) =>
+// `<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>README</title>
+// </head>
+// <body>
+//         <div class="container">
+//           <h1 class="display-4">the project is called ${project}</h1>
+//           <h2 class="lead" >the project description is ${description}</h2>
+//           <h3>follow these instructions: ${instructions}</h3>
+//           <ul class="list-group">
+//             <li class="list-group-item">When to use this application: ${usage}</li>
+//             <li class="list-group-item">table of contents: ${contents}</li>
+//             <li class="list-group-item">installation process: ${install}</li>
+//             <li class="list-group-item">contributors:  ${contributors} </li>
+//             <li class="list-group-item">questions: ${questions}</li>
+//           </ul>
+//         </div>
+// </body>
+// </html>`;
+const promptQuestions = () =>
 inquirer
   .prompt([
     {
@@ -88,10 +91,10 @@ inquirer
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-// function generateMarkdown(data) {
-//   return `# ${data.title}
+function generateMarkdown(data) {
+  return `# ${data.title}
 
-// `;
-// }
+`;
+}
 
-// module.exports = generateMarkdown;
+module.exports = generateMarkdown;
